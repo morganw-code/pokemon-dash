1e57075002d4694a11e0d6a9026df37255cd7cff
Morgan Webb <mnbwebb2233@gmail.com>
2020-06-17 09:53:18 +1000

    Move styling to stylesheet

--- src/app.js
+++ src/app.js
@@ -31,17 +31,16 @@ async function displayPokemon() {
     Object.keys(pokemon.sprites).forEach((key) => {
       if (pokemon.sprites[key] !== null) {
         const spriteImgNode = document.createElement("img");
-        spriteImgNode.src = pokemon.sprites[key];
-        spriteImgNode.style = "width: 96px; height: 96px;";
-        // node that holds the sprite and sprite information
-        const node = document.createElement("div");
-        node.style =
-          "display: flex; flex-direction: column; width: 256px; border: 1px solid #ef5350; padding: 1em; align-items: center;";
-        node.appendChild(spriteImgNode);
+        const spriteNode = document.createElement("div");
         const spriteTitle = capitalize(key, "_");
-        node.appendChild(document.createTextNode(spriteTitle));
-        spriteContainer.appendChild(node);
-        spriteNodes.push(node);
+
+        spriteImgNode.src = pokemon.sprites[key];
+        spriteImgNode.id = "sprite-img";
+        spriteNode.id = "sprite-holder";
+        spriteNode.appendChild(spriteImgNode);
+        spriteNode.appendChild(document.createTextNode(spriteTitle));
+        spriteContainer.appendChild(spriteNode);
+        spriteNodes.push(spriteNode);
       }
     });
   } else {
